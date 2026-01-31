a9c5a8cc0ba185c98e73b6da7446fd06a0b1f1d8
catchjj <bjjkhh@gmail.com>
2026-01-31 15:57:44 +0800

    Add favicon.png to public folder and update all layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Catch Jiu Jitsu - Taiwan's Premier Jiu Jitsu Academy</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
